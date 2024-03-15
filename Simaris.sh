1d8c26ba9b4839bc6a2c0e223859860821af632b
Giacomo Fulvi <giacomofulvi@MBP-Summer6.local>
2024-03-15 11:28:01 +0100

    added colors

--- Simaris.sh
+++ Simaris.sh
@@ -1,112 +1,144 @@
 #!/bin/sh
-clear
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+
+UKNOWN="[????]"
+SIMARIS="[Simaris]"
+
+print_animated() {
+    local message=$1
+	local color=$2
+	printf "%b" "$color"
+    while IFS= read -r -n1 var;
+	do
+        printf '%s' "$var"
+        sleep 0.015
+    done <<< "$message"
+	printf "%b" "$RESET"
+}
 
 if [ $# -ge 0 ] && [ $# -le 1 ]; then
     case "$1" in
     "")
-        str="[???]: Benvenuto nel mio santuario umano, sono Simaris. Sono l'incaricato alla gestione e analisi di tutti i dati che il laboratorio raccoglie, ad ogni modo... immagino tu sia qui per il test?"
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        clear
+        print_animated "$UKNOWN" "$RED"
+        str=": Benvenuto nel mio santuario umano, sono Simaris. Sono l'incaricato alla gestione e all'analisi di tutti i dati che il laboratorio raccoglie, ad ogni modo... immagino tu sia qui per il test?"
+        print_animated "$str" "$WHITE"
 
         echo
-        sleep 0.03
+        sleep 0.3
         echo
 
-        str="[Simaris]: Visto che non abbiamo tempo da perdere, andrò subito al punto. Per trovare il mio indizio, dovrai analizzare una serie di dati presenti nel santuario..."
-
-
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Visto che non abbiamo tempo da perdere, andrò subito al punto. Per trovare il mio indizio, ossia la Chiave dei Dati, dovrai analizzare una serie di dati presenti nel santuario..."
+        print_animated "$str" "$WHITE"
 
         echo
-        sleep 0.03
+        sleep 0.3
         echo
 
-        str="[Simaris]: Per farlo, avrai bisogno di uno dei miei strumenti più potenti. Si tratta del comando \"grep\" combinato con \"wc -l\". Per unire questi due potenti strumenti, dovrai utilizzare un terzo strumento, ovvero il carattere \"|\"."
-
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": In ciascuna di queste serie di dati troverai un pezzo della chiave che poi tu stesso dovrai comporre..."
+        print_animated "$str" "$WHITE"
 
         echo
-        sleep 0.03
+        sleep 0.3
         echo
 
-        str="[Simaris]: Visto che probabilmente non conosci questi strumenti, te li spiegherò io. Grep ti permetterà di trovare le occorrenze di una parola in un file, mentre wc -l ti aiuterà a contare il numero totale di righe in cui l'occorrenza è presente."
-
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
-
-        echo
-        sleep 0.03
-        echo
-
-        str="[Simaris]: Capisco che potresti essere un po' confuso, quindi ho preparato per te un piccolo esempio: \"grep <cosa_vuoi_cercare> <file_in_cui_vuoi_cercare>\" | wc -l. Se questo non fosse sufficiente, ho anche preparato un esempio pratico che puoi utilizzare in qualsiasi momento: basta eseguire \"sh example.sh\"."
-
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Per farlo, avrai bisogno di uno dei miei strumenti più potenti. Si tratta del comando "
+        print_animated "$str" "$WHITE"
+        print_animated "grep" "$PURPLE"
+        str=" combinato con "
+        print_animated "$str" "$WHITE"
+        print_animated "wc -l" "$PURPLE"
+        str=". Per unire questi due potenti strumenti, dovrai utilizzare un terzo strumento, ovvero il carattere "
+        print_animated "$str" "$WHITE"
+        print_animated "|" "$PURPLE"
+        print_animated "." "$WHITE"
 
         echo
-        sleep 0.03
+        sleep 0.3
         echo
 
-        str="[Simaris]: Una volta capito come usare questi strumenti, puoi cominciare a cercare nei 2 dataset che ho preparato appositamente per te, buona fortuna :)"
-
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Visto che probabilmente non conosci questi strumenti, te li spiegherò io: "
+        print_animated "$str" "$WHITE"
+        print_animated "grep" "$PURPLE"
+        str=" ti permetterà di trovare le occorrenze di una parola in un file, mentre "
+        print_animated "$str" "$WHITE"
+        print_animated "wc -l" "$PURPLE"
+        str=" ti aiuterà a contare il numero totale di righe in cui l'occorrenza è presente."
+        print_animated "$str" "$WHITE"
 
         echo
-        sleep 0.03
+        sleep 0.3
         echo
 
-        str="[Simaris]: Se necessiti di ulteriori aiuti, posso darti qualche altro piccolo indizio. Nel caso in cui ti servisse, chiamami specificando il nome del dataset su cui hai difficoltà (esempio: sh simaris.sh 1). Buona fortuna!"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Capisco che potresti essere un po' confuso, quindi ho preparato per te un piccolo esempio: "
+        print_animated "$str" "$WHITE"
+        print_animated "grep <cosa_vuoi_cercare> <file_in_cui_vuoi_cercare> | wc -l" "$PURPLE"
+        str=". Se questo non fosse sufficiente, ho anche preparato un esempio pratico che puoi utilizzare in qualsiasi momento: basta eseguire "
+        print_animated "$str" "$WHITE"
+        print_animated "sh example.sh" "$PURPLE"
+        print_animated "." "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-        	printf '%s' "$var"
-        	sleep 0.015
-        done <<< "$str"
+        echo
+        sleep 0.3
+        echo
 
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Una volta capito come usare questi strumenti, puoi cominciare a cercare 2 nei dataset che ho preparato appositamente per te, buona fortuna :)"
+        print_animated "$str" "$WHITE"
+
+        echo
+        sleep 0.3
+        echo
+
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Giusto per essere chiari, i due dataset in questione sono: "
+        print_animated "$str" "$WHITE"
+        print_animated "simaris_data1.txt" "$YELLOW"
+        print_animated " - " "$WHITE"
+        print_animated "simaris_data2.txt" "$YELLOW"
+        print_animated " ;)" "$WHITE"
+
+        echo
+        sleep 0.3
+        echo
+
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Se necessiti di ulteriori aiuti, posso darti qualche altro piccolo indizio. Nel caso in cui ti servisse, chiamami specificando il nome del dataset su cui hai difficoltà (esempio: "
+        print_animated "$str" "$WHITE"
+        print_animated "sh Simaris.sh 1" "$PURPLE"
+        str="). Buona fortuna!"
+        print_animated "$str" "$WHITE"
+
+        echo
         echo
     ;;
     1)
-        str="[Simaris]: Per capire cosa cercare nel primo dataset ti consiglio di rileggere attentamente ciò che ti avevo scritto precedentemente nel mio penultimo messaggio"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Per capire cosa cercare nel primo dataset ti consiglio di rileggere attentamente ciò che ti avevo scritto precedentemente nel mio penultimo messaggio."
+        print_animated "$str" "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-       	    printf '%s' "$var"
-           	sleep 0.015
-        done <<< "$str"
-
+        echo
         echo
     ;;
     2)
-        str="[Simaris]: Per capire cosa cercare nel primo dataset ti consiglio di comprendere l'argomento di cui si parla nel secondo dataset"
+        print_animated "$SIMARIS" "$GREEN"
+        str=": Per capire cosa cercare nel secondo dataset ti consiglio di comprendere l'argomento principale di cui si parla al suo interno."
+        print_animated "$str" "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-       	    printf '%s' "$var"
-           	sleep 0.015
-        done <<< "$str"
-
+        echo
         echo
     ;;
     *)
